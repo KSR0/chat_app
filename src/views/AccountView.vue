38a41eec0d3138494d53677df7323722a55b9d5f
Nicolas <nicolas.peyregne@etu.unicaen.fr>
2024-01-25 10:24:25 +0100

    feat: add tailwind

---
+++ src/views/AccountView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-blue-400">My account</div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-orange-900">
+        Hello World !
+    </div>
+</template>
+
